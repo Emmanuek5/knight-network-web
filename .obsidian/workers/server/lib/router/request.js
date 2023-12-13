66c7bfb9a4879a20a0d2f73f895a9fc50952e32a
Emmanuek5 <emmanuelandrew625@yahoo.com>
2023-12-13 19:44:23 +0100

    Update request

--- .obsidian/workers/server/lib/router/request.js
+++ .obsidian/workers/server/lib/router/request.js
@@ -35,6 +35,7 @@ class Request {
     const contentType = this.headers["content-type"];
 
     if (contentType && contentType.includes("application/json")) {
+      this.body = this.toJSON();
     } else if (
       contentType &&
       contentType.includes("application/x-www-form-urlencoded")
